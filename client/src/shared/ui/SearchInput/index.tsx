80d87f40844aad85fe9237632cefcf891e5c457e
Pavel <pavelparshin2002005@gmail.com>
2024-12-19 15:40:36 +0300

    feat: add search input component

---
+++ client/src/shared/ui/SearchInput/index.tsx
@@ -0,0 +1,35 @@
+// src/shared/SearchInput/SearchInput.tsx
+import React from "react";
+import styles from "./SearchInput.module.scss";
+import searchIcon from "../../../assets/icons/search-icon.svg";
+
+interface SearchInputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  className?: string;
+}
+
+const SearchInput: React.FC<SearchInputProps> = ({
+  value,
+  onChange,
+  placeholder,
+  className,
+}) => {
+  return (
+    <div className={`${styles.searchInputWrapper} ${className}`}>
+      <div className={styles.iconWrapper}>
+        <img src={searchIcon} className={styles.icon} />
+      </div>
+      <input
+        type="text"
+        className={styles.searchInput}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
